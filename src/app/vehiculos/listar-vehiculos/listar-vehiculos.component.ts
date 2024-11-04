31966698e47e7bd4c3bda79e72ebcbc3ca24918f
Jhoan Sarmiento <jf.sarmiento23@uniandes.edu.co>
2024-11-03 20:32:48 -0500

    Agregar clase Vehiculo

---
+++ src/app/vehiculos/listar-vehiculos/listar-vehiculos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-vehiculos',
+  standalone: true,
+  imports: [],
+  templateUrl: './listar-vehiculos.component.html',
+  styleUrl: './listar-vehiculos.component.css'
+})
+export class ListarVehiculosComponent {
+
+}
